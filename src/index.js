b51a4f7839ad468ea9cc587b631178878b797db1
raxoors <ra@xoors.com>
2017-05-18 01:10:22 +0800

    Initial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-export {momentFromNow, dateToday, dateTomorrow, dateDayAfterTomorrow} from './moment'
-
+export {momentFromNow, today, tomorrow, dayAfterTomorrow, dayOfWeek, dayOfMonth, month, localizedDateTime, isBeforeToday, isAfterToday, isToday} from './moment'
 export {isEmail, isValid} from './validator'
--- src/moment/index.js
+++ src/moment/index.js
@@ -1,17 +1,83 @@
 import moment from 'moment'
 
 export function momentFromNow(isoDateTime) {
-  return moment(isoDateTime).fromNow()
+  return moment(isoDateTime).fromNow() //E.g. 2 months ago, a few seconds ago
 }
 
-export function dateToday() {
-  return moment.utc(new Date()).format('YYYY-MM-DD')
+export function today() {
+  return moment.utc(new Date()).format('YYYY-MM-DD') //E.g. Sat May 13 2017 08:00:00 GMT+0800 (CST)
 }
 
-export function dateTomorrow() {
-  return moment.utc(new Date()).add(1, 'd').format('YYYY-MM-DD')
+export function tomorrow() {
+  return moment.utc(new Date()).add(1, 'd').format('YYYY-MM-DD') //E.g. Sun May 14 2017 08:00:00 GMT+0800 (CST)
 }
 
-export function dateDayAfterTomorrow() {
-  return moment.utc(new Date()).add(2, 'd').format('YYYY-MM-DD')
+export function dayAfterTomorrow() {
+  return moment.utc(new Date()).add(2, 'd').format('YYYY-MM-DD') //E.g. Mon May 15 2017 08:00:00 GMT+0800 (CST)
+}
+
+export function dayOfWeek(isoDateTime, format) {
+  let result
+  switch (format) {
+    case 'short': {
+      result = moment(isoDateTime).format('ddd')
+      break
+    }
+    default: {
+      result = moment(isoDateTime).format('dddd')
+    }
+  }
+  return result
+}
+
+export function dayOfMonth(isoDateTime, format) {
+  return moment(isoDateTime).format('D')
+}
+
+export function month(isoDateTime, format) {
+  let result
+  switch (format) {
+    case 'short': {
+      result = moment(isoDateTime).format('MMM')
+      break
+    }
+    default: {
+      result = moment(isoDateTime).format('MMMM')
+    }
+  }
+  return result
+}
+
+export function localizedDateTime(isoDateTime, format) {
+  let result
+  switch (format) {
+    case 'shortdate': {
+      result = moment(isoDateTime).format('ll')
+      break
+    }
+    case 'longdate': {
+      result = moment(isoDateTime).format('LL')
+      break
+    }
+    default: {
+      result = moment(isoDateTime).format('LLLL')
+    }
+  }
+  return result
+}
+
+export function isBeforeToday(isoDateTime) {
+  return moment(isoDateTime).isBefore(today())
+}
+
+export function isAfterToday(isoDateTime) {
+  return moment(isoDateTime).isAfter(today())
+}
+
+export function isToday(isoDateTime) {
+  if (moment(isoDateTime).diff(today(), 'days') === 0) {
+    return true
+  } else {
+    return false
+  }
 }
