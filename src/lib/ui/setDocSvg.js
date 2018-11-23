9d27c84131d72149840e2bd241d82f5aa3a76db7
Rusli Ang <ra@xoors.com>
2018-11-23 22:22:58 +0800

    Initial

--- src/lib/ui/setDocSvg.js
+++ src/lib/ui/setDocSvg.js
@@ -1,9 +1,10 @@
 const setDocSvg = (id, svg) => {
   if (svg) {
-    const body = document.getElementsByTagName('body')[0]
     if (document.getElementById(id) === null) {
-      const newSvg = document.createElement('svg')
+      const body = document.getElementsByTagName('body')[0]
+      const newSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
       newSvg.setAttribute('id', id)
+      newSvg.setAttribute('style', 'display:none;')
       newSvg.innerHTML = svg
       body.appendChild(newSvg)
     } else {
